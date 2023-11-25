d87cc26ed5e77f934440c0f7919495580b28ed38
NurAtika19 <nuratika190103@gimail.com>
2023-11-25 12:07:16 +0700

    menambahkan halaman private

--- resources/views/halaman-kodepos/privasi.blade.php
+++ resources/views/halaman-kodepos/privasi.blade.php
@@ -1,4 +1,67 @@
 @extends('layout.nav')
-@section('content')
 
+@section('content')
+    <link rel="stylesheet" href="/css/style.css">
+        <div class="container">
+                 {{-- <header class="header clear" role="banner"> --}}
+                     <div class="logo">
+                         <a href="#">
+                             <img src="https://carikodepos.com/wp-content/themes/kodepos5/img/carikodepos.png" height="100" width="350" title="CariKodePOS.com" alt="Logo Kode Pos" class="logo-img" scale="0">
+                         </a>
+                     </div>
+             </div>
+        </div>
+        <br class="clear">
+        <main role="main">
+                 <div class="wrapper">
+                    <section>
+                    <article id="post-81260" class="post-81260 page type-page status-publish hentry">  
+                    <p><strong>Kebijakan privasi</strong></p>
+                     <p>Privasi para pengunjung carikodepos.com ini sangat penting. Dokumen kebijakan privasi ini mengurai jenis informasi pribadi yang diterima dan dikumpulkan oleh carikodepos.com dan bagaimana informasi pribadi tersebut digunakan.</p>                            
+                       
+                         
+                        <br>
+                            <p><strong>Log Files</strong></p>
+                            <p>Seperti kebanyakan situs web lain, carikodepos.com juga menggunakan log files. Informasi dalam log files meliputi alamat internet protocol (IP), jenis browser, Internet Service Provider (ISP), jejak tanggal/waktu, refferering atau exit pages dan klik-klik yang dilakukan pengunjung untuk menganalisis kecenderungan, mengelola situs, melacak aktifitas pengguna pada situs, dan untuk mengumpulkan informasi demografis. Alamat IP dan informasi lainnya tersebut tidak terkait dengan informasi yang bersifat pribadi.</p>
+                           
+                        <br>
+                         
+                            <p><strong>Cookies dan Web Beacons</strong></p>
+                            <p>Carikodepos.com menggunakan cookies untuk menyimpan informasi tentang preferensi pengunjung, merekam informasi pengguna tertentu pada halaman yang dikunjungi, menyesuaikan halaman web berdasarkan tipe browser yang digunakan pengunjung atau informasi lainnya yang pengunjung kirimkan melalui browser yang digunakan.</p>
+                        <br>
+                            <p><strong>DoubleClick DART Cookie</strong></p>
+                            <ul>
+                                <li>Google, sebagai vendor pihak ketiga, menggunakan cookies untuk menayangkan iklan di carikodepos.com.</li>
+                                <li>Penggunaan DART cookie oleh Google memungkinkan Google dapat menampilkan iklan yang sesuai kepada pengguna berdasarkan kunjungan di carikodepos.com dan situs lainnya di Internet.</li>
+                                <li>Pengguna dapat membatalkan penggunaan DART cookie dengan mengunjungi kebijakan privasi jaringan iklan dan konten Google di URL berikut : http://www.google.com/policies/technologies/ads/</li>
+                            </ul>
+                                
+                        <br>
+                            <p>Beberapa mitra periklanan kami mungkin menggunakan cookies dan web beacon di situs. Mitra iklan kami meliputi : Google Adsense & Infolinks.</p>
+                        <br>
+                            <p>Server-server iklan pihak ketiga atau jaringan iklan ini menggunakan teknologi untuk mengirim iklan-iklan dan link-link yang muncul di carikodepos.com secara langsung ke browser Anda. Mereka secara otomatis menerima alamat IP Anda. Teknologi lainnya (seperti cookies, JavaScript atau Web Beacons) juga dapat digunakan oleh jaringan iklan pihak ketiga untuk mengukur efektifitas iklan mereka dan/atau untuk personalisasi iklan yang Anda lihat.</p>
+
+                        <br>
+                            <p>Carikodepos.com tidak memiliki akses atau kontrol terhadap cookies yang digunakan oleh pengiklan pihak ketiga.</p>
+
+                        <br>
+                            <p>Anda harus berkonsultasi dengan kebijakan privasi masing-masing dari server iklan pihak ketiga untuk informasi lebih rinci tentang kegiatan mereka serta untuk mendapat petunjuk tentang cara membatalkan kegiatan-kegiatan tertentu. Kebijakan Privasi kami tidak dapat mengontrol kegiatan tersebut.</p>
+
+                        <br>
+                        <p>Jika Anda ingin menonaktifkan cookies, Anda dapat melakukannya melalui pilihan-pilihan di browser Anda. Informasi lebih lanjut tentang manajemen cookie dengan browser web tertentu dapat ditemukan di situs web masing-masing browser.</p>
+
+                        <br>
+                            <p>Jika Anda memerlukan informasi lebih lanjut atau memiliki pertanyaan lain tentang kebijakan privasi pada website carikodepos.com, silahkan menghubungi kami melalui halaman kontak.</p>
+
+                        <br>
+                        </article>
+                    </section>
+                    
+                </div>           
+            </main>
+   
+    <footer class="footer" role="contentinfo">
+        <p class="copyright"></p>
+    </footer>
+</div>
 @endsection
